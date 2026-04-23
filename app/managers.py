7fc8ec2205a5b820a4a5f7c780b8c9e83928773e
Arseniy Petrenko <arsfix2004@gmail.com>
2026-04-23 14:16:27 +0300

    'Solution'

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,45 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self.cursor.execute(
+            f"INSERT INTO {self.table_name} "
+            "(first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name)
+        )
+        self.connection.commit()
+
+    def all(self) -> list[Actor]:
+        self.cursor.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+
+        rows = self.cursor.fetchall()
+
+        actors = []
+        for row in rows:
+            actors.append(Actor(*row))
+
+        return actors
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        self.cursor.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? WHERE id = ?",
+            (new_first_name, new_last_name, pk)
+        )
+        self.connection.commit()
+
+    def delete(self, pk: int) -> None:
+        self.cursor.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (pk,)
+        )
+        self.connection.commit()
